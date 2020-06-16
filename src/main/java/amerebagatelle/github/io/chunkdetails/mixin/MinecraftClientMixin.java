48b8d19932738d9a8bc753f85146e2b1a7fbe11f
AMereBagatelle <themerebagatelle@gmail.com>
2020-06-16 09:08:57 -0400

    Changed mixin location

--- src/main/java/amerebagatelle/github/io/chunkdetails/mixin/MinecraftClientMixin.java
+++ src/main/java/amerebagatelle/github/io/chunkdetails/mixin/MinecraftClientMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Environment(EnvType.CLIENT)
 @Mixin(MinecraftClient.class)
 public class MinecraftClientMixin {
-    @Inject(method = "tick", at = @At("HEAD"))
+    @Inject(method = "tick", at = @At("RETURN"))
     public void onTick(CallbackInfo ci) {
         ChunkLoadedListScreen.loadedChunks.clear();
     }
